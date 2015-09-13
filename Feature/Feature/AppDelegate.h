861d7180a1fb49756c382469c997b9a9adc2d72c
sa.xiong <sa.xiong@renren-inc.com>
2015-09-13 17:24:25 +0800

    add project

---
+++ Feature/Feature/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Feature
+//
+//  Created by sa.xiong on 15/9/13.
+//  Copyright © 2015年 sa.xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Feature/Feature/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Feature
+//
+//  Created by sa.xiong on 15/9/13.
+//  Copyright © 2015年 sa.xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
